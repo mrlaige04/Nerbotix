7b948fbd34515a2b3b35a144e59b9b1b457ed502
mrlaige <rudiakov.illia@gmail.com>
2025-03-17 19:53:42 +0200

    role system

--- src/RoboTasker.Domain/Tenants/Tenant.cs
+++ src/RoboTasker.Domain/Tenants/Tenant.cs
@@ -8,4 +8,5 @@ public class Tenant : BaseEntity
     public string Email { get; set; } = null!;
     public IList<User> Users { get; set; } = [];
     public IList<Role> Roles { get; set; } = [];
+    public IList<PermissionGroup> PermissionGroups { get; set; } = [];
 }
